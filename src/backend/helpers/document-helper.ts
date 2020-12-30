e385c07247cfeb56a536f0d600753fd93c3c0092
AmirHossein MOjvar <MOjvar.AmirHossein@Gmail.com>
2020-12-30 19:45:46 +0330

    Remove console.log

--- src/backend/helpers/document-helper.ts
+++ src/backend/helpers/document-helper.ts
@@ -40,8 +40,6 @@ export default class DocumentHelper {
             "Document"
         );
 
-            console.log(doc)
-
         const result = await Document.updateOne(
             {
                 _id: Mongoose.Types.ObjectId(doc.id),
